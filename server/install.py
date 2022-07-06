4cde29157c9e4afc1cdb4a92bb9fa74c2742de02
Miguel D. Guerrero <migs@mdguerrero.com>
2022-07-06 13:13:54 -0500

    Windows changes

--- server/install.py
+++ server/install.py
@@ -1,6 +1,6 @@
 import os, PyInstaller.__main__
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-INSTALL_FILE = os.path.join(THIS_FOLDER, 'server.py')
+INSTALL_FILE = os.path.join(THIS_FOLDER, 'rbk_server.py')
 
 PyInstaller.__main__.run([
     INSTALL_FILE,
